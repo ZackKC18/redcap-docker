bb7de1f68c11ed3670ea6911e8e68bafd3ef8d03
ZackKC18 <79562615+ZackKC18@users.noreply.github.com>
2021-10-18 15:03:02 +0700

    Update database.php

--- web/database.php
+++ web/database.php
@@ -5,7 +5,7 @@
 // Replace the values inside the single quotes below with the values for your MySQL configuration. 
 // If not using the default port 3306, then append a colon and port number to the hostname (e.g. $hostname = 'example.com:3307';).
 
-$hostname 	= 'your_mysql_host_name';
+$hostname 	= 'redcap-db';
 $db 		= 'your_mysql_db_name';
 $username 	= 'admin';
 $password 	= 'password';
